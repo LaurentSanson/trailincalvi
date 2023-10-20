e83e639e103d9bfdb2fda4399e38e245d5d5af8f
laurent <laurent.sanson@trusthq.com>
2023-10-20 18:11:16 +0200

    fix: updated variables for letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(lescopainsdutrail.fr www.lescopainsdutrail.fr)
+domains=(trailincalvi.corsica www.trailincalvi.corsica)
 rsa_key_size=4096
 data_path=".docker/certbot"
 email="laurentsanson.pro@gmail.com" # Adding a valid address is strongly recommended
